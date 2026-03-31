c720a83a499dccfc729d939bdd5dad068036ec54
A V Jefferson <avjeferson@gmail.com>
2026-03-31 06:20:47 +0000

    move app location to /sdrive. change to build-dev and build-prd for svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { invoke } from "@tauri-apps/api/core";
+  import { asset } from '$app/paths';
 
   let name = $state("");
   let greetMsg = $state("");
@@ -16,13 +17,13 @@
 
   <div class="row">
     <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
+      <img src={asset("/vite.svg")} class="logo vite" alt="Vite Logo" />
     </a>
     <a href="https://tauri.app" target="_blank">
-      <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
+      <img src={asset("/tauri.svg")} class="logo tauri" alt="Tauri Logo" />
     </a>
     <a href="https://svelte.dev" target="_blank">
-      <img src="/svelte.svg" class="logo svelte-kit" alt="SvelteKit Logo" />
+      <img src={asset("/svelte.svg")} class="logo svelte-kit" alt="SvelteKit Logo" />
     </a>
   </div>
   <p>Click on the Tauri, Vite, and SvelteKit logos to learn more.</p>

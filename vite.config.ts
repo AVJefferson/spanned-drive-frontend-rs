627d7651dc2a9b93f696283cac54a80eda683447
A V Jefferson <avjefferson@bajajauto.co.in>
2026-04-03 16:25:24 +0000

    move to react for component library support

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import { sveltekit } from "@sveltejs/kit/vite";
+import react from "@vitejs/plugin-react";
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
@@ -20,7 +20,7 @@ function clearConsole() {
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-  plugins: [sveltekit(), clearConsole()],
+  plugins: [react(), clearConsole()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
@@ -28,14 +28,14 @@ export default defineConfig(async () => ({
   clearScreen: false,
   // 2. tauri expects a fixed port, fail if that port is not available
   server: {
-    port: 3000,
+    port: 1420,
     strictPort: true,
-    host: host || "0.0.0.0",
+    host: host || false,
     hmr: host
       ? {
           protocol: "ws",
           host,
-          port: 3000,
+          port: 1421,
         }
       : undefined,
     watch: {

05a0ff0dd7ca0b3eb819be5bc6e199d2a6b888a2
A V Jefferson <avjefferson@bajajauto.co.in>
2026-04-03 06:26:02 +0000

    add pane files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
+  import NavPane from "$lib/components/NavPane.svelte";
+  import ContentPane from "$lib/components/ContentPane.svelte";
+  import InfoPane from "$lib/components/InfoPane.svelte";
 </script>
 
-<main class="container"></main>
-
-<style>
-</style>
+<main class="flex h-screen w-screen overflow-hidden">
+  <div class="flex-none w-64 bg-gray-100 border-r border-gray-300">
+    <NavPane />
+  </div>
+  <div class="flex-1 bg-white">
+    <ContentPane />
+  </div>
+  <div class="flex-none w-52 bg-gray-200 border-l border-gray-300">
+    <InfoPane />
+  </div>
+</main>

bbe8775e064470e4cb281a3fc67dabe2d0465077
A V Jefferson <avjefferson@bajajauto.co.in>
2026-04-03 05:42:26 +0000

    add clearConsole plugin. change def port to 3000

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,23 @@ import { sveltekit } from "@sveltejs/kit/vite";
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
+function clearConsole() {
+  return {
+    name: "clear-console",
+    // Clears on file save (Hot Module Replacement)
+    handleHotUpdate() {
+      console.clear();
+    },
+    // Clears on initial server start or full restart
+    buildStart() {
+      console.clear();
+    },
+  };
+}
+
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-  plugins: [sveltekit()],
+  plugins: [sveltekit(), clearConsole()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
@@ -14,14 +28,14 @@ export default defineConfig(async () => ({
   clearScreen: false,
   // 2. tauri expects a fixed port, fail if that port is not available
   server: {
-    port: 80,
+    port: 3000,
     strictPort: true,
     host: host || "0.0.0.0",
     hmr: host
       ? {
           protocol: "ws",
           host,
-          port: 80,
+          port: 3000,
         }
       : undefined,
     watch: {

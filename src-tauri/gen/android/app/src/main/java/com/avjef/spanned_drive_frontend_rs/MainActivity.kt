8d60c0304aabdd3a0a50d52adf20542950674209
A V Jefferson <avjeferson@gmail.com>
2026-04-16 23:18:41 +0530

    add build for android

---
+++ src-tauri/gen/android/app/src/main/java/com/avjef/spanned_drive_frontend_rs/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.avjef.spanned_drive_frontend_rs
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}

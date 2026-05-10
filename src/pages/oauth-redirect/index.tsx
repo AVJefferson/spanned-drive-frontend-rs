485fceaa4f978fb001a032197ce91c64d5985366
A V Jefferson <avjefferson@bajajauto.co.in>
2026-05-10 04:01:29 +0000

    useRef to ensure oauth is invoked only once

--- src/pages/oauth-redirect/index.tsx
+++ src/pages/oauth-redirect/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import {
   STORAGE_KEYS,
@@ -11,64 +12,78 @@ export const OauthRedirectPages = () => {
   const { provider } = useParams();
   const session = useSession();
   const navigate = useNavigate();
+  const calledRef = useRef(false);
 
-  if (!provider) return <h1>Invalid Provider</h1>;
+  useEffect(() => {
+    if (calledRef.current) return;
+    calledRef.current = true;
 
-  const DriveImplementation = getDriveImplementation(provider);
-  if (!DriveImplementation?.oauthCallback) {
-    return <h1>Provider not found</h1>;
-  }
-
-  const queryParams = new URLSearchParams(window.location.search);
-  const hashParams = new URLSearchParams(window.location.hash.substring(1));
-
-  const oauthParamsRaw = localStorage.getItem(STORAGE_KEYS.oauthParams);
-
-  const params = {
-    provider,
-    queryParams: Object.fromEntries(queryParams.entries()),
-    hashParams: Object.fromEntries(hashParams.entries()),
-    oauthParams: oauthParamsRaw ? JSON.parse(oauthParamsRaw) : {},
-  };
-
-  const oauthParamsOnce = readLocalStorageJson(
-    STORAGE_KEYS.oauthParams + "-once",
-    0,
-  );
-  if (
-    oauthParamsOnce &&
-    oauthParamsOnce > Date.now() - 1000 * 60 * 5 /** 5 minutes */
-  ) {
-    return <h1>OAuth callback already processed. Please wait 5 minutes before trying again.</h1>;
-  }
-  writeLocalStorageJson(STORAGE_KEYS.oauthParams + "-once", Date.now());
-
-  DriveImplementation.oauthCallback(params).then((result) => {
-    writeLocalStorageJson(STORAGE_KEYS.oauthParams + "-once", 0);
-    if (
-      !result.success ||
-      !result.provider ||
-      !result.email ||
-      !result.refreshToken
-    )
-      navigate("/error?error=OAuth Callback Failed. Please try again.");
-    else {
-      if (result.accountType === "primary") {
-      session.setPrimaryDrive(
-          result.provider,
-          result.email,
-          result.refreshToken,
-        );
-      } else {
-        session.addSecondaryDrive(
-          result.provider,
-          result.email,
-          result.refreshToken,
-        );
-      }
-      navigate("/");
+    if (!provider) {
+      navigate("/error?error=Invalid Provider");
+      return;
     }
-  });
+
+    const DriveImplementation = getDriveImplementation(provider);
+    if (!DriveImplementation?.oauthCallback) {
+      navigate("/error?error=Provider not found");
+      return;
+    }
+
+    const queryParams = new URLSearchParams(window.location.search);
+    const hashParams = new URLSearchParams(window.location.hash.substring(1));
+
+    const oauthParamsRaw = localStorage.getItem(STORAGE_KEYS.oauthParams);
+
+    const params = {
+      provider,
+      queryParams: Object.fromEntries(queryParams.entries()),
+      hashParams: Object.fromEntries(hashParams.entries()),
+      oauthParams: oauthParamsRaw ? JSON.parse(oauthParamsRaw) : {},
+    };
+
+    const oauthParamsOnce = readLocalStorageJson(
+      STORAGE_KEYS.oauthParams + "-once",
+      0,
+    );
+    if (
+      oauthParamsOnce &&
+      oauthParamsOnce > Date.now() - 1000 * 60 * 5 /** 5 minutes */
+    ) {
+      navigate(
+        "/error?error=OAuth Callback Already Processed. Please wait 5 minutes before trying again.",
+      );
+      return;
+    }
+    writeLocalStorageJson(STORAGE_KEYS.oauthParams + "-once", Date.now());
+
+    DriveImplementation.oauthCallback(params).then((result) => {
+      if (
+        !result.success ||
+        !result.provider ||
+        !result.email ||
+        !result.refreshToken
+      ) {
+        writeLocalStorageJson(STORAGE_KEYS.oauthParams + "-once", 0);
+        navigate("/error?error=OAuth Callback Failed. Please try again.");
+      } else {
+        if (result.accountType === "primary") {
+          session.setPrimaryDrive(
+            result.provider,
+            result.email,
+            result.refreshToken,
+          );
+        } else {
+          session.addSecondaryDrive(
+            result.provider,
+            result.email,
+            result.refreshToken,
+          );
+        }
+        navigate("/");
+      }
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return <h1>Please wait while we are processing OAuth callback...</h1>;
 };
